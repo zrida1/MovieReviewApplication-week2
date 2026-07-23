54220dda8301a6aa7e0a9f7d81362b54b9d1c251
zrida1 <ragimovazrida@gmail.com>
2026-07-23 21:16:49 +0400

    CHECKPOINT1_User_entity_password_hashing

--- src/main/java/com/example/moviewreviewapplication/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/moviewreviewapplication/service/impl/UserServiceImpl.java
@@ -12,12 +12,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
     private final UserRepository userRepository;
     private final UserMapper userMapper;
     public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
@@ -34,13 +36,14 @@ public class UserServiceImpl implements UserService {
     }
 
     public UserResponseDTO createUser(UserRequestDTO userRequestDTO){
+        userRequestDTO.setPassword(encoder.encode(userRequestDTO.getPassword()));
         return userMapper.toResponseDTO(userRepository.save(userMapper.toEntity(userRequestDTO)));
     }
     public UserResponseDTO updateUser(Long id, UserRequestDTO userRequestDTO){
         User user = userRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("User not found with id: " + id));
         user.setName(userRequestDTO.getName());
         user.setEmail(userRequestDTO.getEmail());
-        user.setPassword(userRequestDTO.getPassword());
+        user.setPassword(encoder.encode(userRequestDTO.getPassword()));
         return userMapper.toResponseDTO(userRepository.save(user));
     }
     public void deleteUser(Long id){
